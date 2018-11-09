0376e4cd37d0ba4190e37bf57e6a4f28fce0b33f
Paul Antoine Hocke <hocke.p.a@gmail.com>
2018-11-09 16:09:57 +0100

    mister cocktail terminated

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,39 @@
+class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create, :updated_at, :destroy]
+  before_action :set_dose, only: [:destroy]
+  def new
+    @dose = Dose.new
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose.destroy
+    # @dose.cocktail = Cocktail.find(@dose[:cocktail_id])
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
+
+  resources :cocktails, only: [:show, :new, :create, :update, :destroy] do
+    resources :doses, only: [:index, :new, :create]
+  end
+
+  resources :doses, only: [:destroy]
+  resources :ingredients, only: [:index]
 end
